61bd002ce78be3e25f03a1ee24636f97ce911f61
ly-wonderful <59376378+ly-wonderful@users.noreply.github.com>
2026-05-10 18:02:24 -0500

    Delete SimplyWorkout directory

--- SimplyWorkout/SimplyWorkout/Models/LoggedSet.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct LoggedSet: Codable, Identifiable {
-    var id: UUID = UUID()
-    var reps: Int
-    var weight: Double
-}
--- SimplyWorkout/SimplyWorkout/Views/Auth/AuthGateView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct AuthGateView: View {
-    var body: some View {
-        NavigationStack {
-            SignInView()
-        }
-    }
-}
